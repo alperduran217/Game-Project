ace5f5d76eb2de48e801e55f728e9025b08a6901
alperduran217 <alperduran217@gmail.com>
2016-05-06 17:07:14 +0300

    tile map pics and json are added

--- Screens/ScreenManager.js
+++ Screens/ScreenManager.js
@@ -5,6 +5,13 @@ var gameHeight = 640;
 var player;
 function preload() {
 
+    //!-- TILE MAP, PICS, AND JSON --!//
+    game.load.tilemap('introLevel', 'Assets/TileMaps/introLevel.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tilesheet_complete','Assets/Tilemaps/introLevelForeground.png');
+    game.load.image('set4_background','Assets/Tilemaps/introLevelBackground_2.png');
+    game.load.image('set1_tiles','Assets/Tilemaps/introLevelBackground_1.png');
+
+
     
 
 }
