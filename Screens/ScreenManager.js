69aa97f63e085d934ff50d227a455b4b04c80c1b
alperduran217 <alperduran217@gmail.com>
2016-05-06 16:53:09 +0300

    First Commit
    Created intro Level and other important files

---
+++ Screens/ScreenManager.js
@@ -0,0 +1,30 @@
+var game = new Phaser.Game(900, 640, Phaser.AUTO, '', { preload: preload, create: create, update: update, });
+var gameWidth = 900;
+var gameHeight = 640;
+
+var player;
+function preload() {
+
+    
+
+}
+
+
+
+function create() {
+
+
+    
+
+}
+
+
+
+
+function update() {
+
+    
+
+}
+
+
