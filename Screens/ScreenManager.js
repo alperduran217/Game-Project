5d774859c015b9cfa87efaec775050fcf3da7c24
alperduran217 <alperduran217@gmail.com>
2016-05-06 17:14:40 +0300

    game physic are added

--- Screens/ScreenManager.js
+++ Screens/ScreenManager.js
@@ -3,6 +3,12 @@ var gameWidth = 900;
 var gameHeight = 640;
 
 var player;
+var map;
+var cursors;
+var layer;
+var CollideLayer;
+var tileset;
+
 function preload() {
 
     //!-- TILE MAP, PICS, AND JSON --!//
@@ -11,17 +17,25 @@ function preload() {
     game.load.image('set4_background','Assets/Tilemaps/introLevelBackground_2.png');
     game.load.image('set1_tiles','Assets/Tilemaps/introLevelBackground_1.png');
 
-
-    
-
 }
 
 
 
 function create() {
 
-
+    game.physics.StartSystem(Phaser.Physics.ARCADE);
     
+    cursors = game.input.keyboard.createCursorsKeys();
+    
+    map = game.add.tilemap('introLevel');
+    
+    layer = map.createLayer('Background');
+    
+    CollideLayer = map.createLayer('Foreground');
+    
+    map.setCollisionBetween(0, 180, true, CollideLayer);
+    
+    layer.resizeWorld();
 
 }
 
